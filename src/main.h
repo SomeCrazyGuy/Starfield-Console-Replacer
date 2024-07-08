552d6f606fc91d6c2eb2086a0e2e59777e3869c1
linuxversion <1660477+SomeCrazyGuy@users.noreply.github.com>
2024-07-08 17:31:02 -0400

    prevent unnecessary error on Alt+F4, add new apis to betterapi.h, bump version to 1.4.2

--- src/main.h
+++ src/main.h
@@ -1,55 +1,16 @@
 #pragma once
 
-#include <stdlib.h>
-#include <stdint.h>
-
 #define MODMENU_DEBUG
-
-#ifndef MODMENU_DEBUG
-#ifdef _DEBUG
-#define MODMENU_DEBUG
-#endif // _DEBUG
-#endif // !MODMENU_DEBUG
-
-#ifdef MODMENU_DEBUG
-
-constexpr const char* next_slash(const char* const path) {
-        return (!path)
-                ? nullptr
-                : (*path == '/' || *path == '\\')
-                ? path
-                : (*path == 0)
-                ? nullptr
-                : next_slash(path + 1);
-}
-
-constexpr const char* filename_only(const char* const path) {
-        return (next_slash(path))
-                ? filename_only(next_slash(path + 1))
-                : path;
-}
-
-extern void DebugImpl(const char* const filename, const char* const func, int line, const char* const fmt, ...) noexcept;
-extern void AssertImpl(const char* const filename, const char* const func, int line, const char* const text) noexcept;
-extern void TraceImpl(const char* const filename, const char* const func, int line, const char* const fmt, ...) noexcept;
-#define DEBUG(...) do { DebugImpl(filename_only(__FILE__), __func__, __LINE__, " " __VA_ARGS__); } while(0)
-#define ASSERT(CONDITION) do { if (!(CONDITION)) { AssertImpl(filename_only(__FILE__), __func__, __LINE__, " " #CONDITION); } } while(0)
-#define TRACE(...) do { TraceImpl(filename_only(__FILE__), __func__, __LINE__, " " __VA_ARGS__); } while(0)
-#define IMGUI_DEBUG_PARANOID
-#else
-#define DEBUG(...) do { } while(0)
-#define ASSERT(...) do { } while(0)
-#defince TRACE(...) do { } while(0)
-#endif // MODMENU_DEBUG
-
-
 #define WIN32_LEAN_AND_MEAN
 #define VC_EXTRA_LEAN
+
+#include <stdlib.h>
+#include <stdint.h>
 
 #define BETTERAPI_ENABLE_SFSE_MINIMAL
 #include "../betterapi.h"
 
-
+#include "debug_log.h"
 #include "callback.h"
 #include "hook_api.h"
 #include "simpledraw.h"
@@ -60,19 +21,11 @@ extern void TraceImpl(const char* const filename, const char* const func, int li
 #include "../imgui/imgui.h"
 
 
-#define BETTERCONSOLE_VERSION "1.4.1"
+#define BETTERCONSOLE_VERSION "1.4.2"
 #define COMPATIBLE_GAME_VERSION "1.12.36"
 
-// --------------------------------------------------------------------
-// ---- Change these offsets for each game update                  ----
-// --------------------------------------------------------------------
-constexpr uint32_t GAME_VERSION = MAKE_VERSION(1, 12, 36);
+constexpr uint32_t GAME_VERSION = BC_MAKE_VERSION(1, 12, 36);
 
-
-
-//---------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------
 struct ModMenuSettings {
         uint32_t FontScaleOverride = 0;
 };
